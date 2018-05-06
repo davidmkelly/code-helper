42212e7f1dc3fd1720eba582795fe1d87ef947d3
David Kelly <kellydmk0o0@gmail.com>
2018-05-05 23:24:10 -0500

    Initial commit

---
+++ src/main/java/info/davidmkelly/CodeHelperApplication.java
@@ -0,0 +1,12 @@
+package info.davidmkelly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeHelperApplication.class, args);
+    }
+}
